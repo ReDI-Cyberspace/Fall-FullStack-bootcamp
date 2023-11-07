e456e9e772a5cef6e1db38d587fb4119020c0c7f
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2023-11-07 23:05:32 +0100

    event listeners creade and linked to functions

--- Portfolio/public/gallery/scripts.js
+++ Portfolio/public/gallery/scripts.js
@@ -1,51 +1,3 @@
-// const products = [
-//     { id: 1, size: "S", color: "Black", type: "T-shirt" },
-//     { id: 2, size: "M", color: "Black", type: "T-shirt" },
-//     { id: 3, size: "L", color: "Black", type: "T-shirt" },
-//     { id: 4, size: "XL", color: "Black", type: "T-shirt" },
-//     { id: 5, size: "S", color: "Blue", type: "T-shirt" },
-//     { id: 6, size: "M", color: "Blue", type: "T-shirt" },
-//     { id: 7, size: "L", color: "Blue", type: "T-shirt" },
-//     { id: 8, size: "XL", color: "Blue", type: "T-shirt" },
-//     { id: 9, size: "S", color: "Red", type: "T-shirt" },
-//     { id: 10, size: "M", color: "Red", type: "T-shirt" },
-//     { id: 11, size: "L", color: "Red", type: "T-shirt" },
-//     { id: 12, size: "XL", color: "Red", type: "T-shirt" },
-//     { id: 13, size: "S", color: "Olive", type: "T-shirt" },
-//     { id: 14, size: "M", color: "Olive", type: "T-shirt" },
-//     { id: 15, size: "L", color: "Olive", type: "T-shirt" },
-//     { id: 16, size: "XL", color: "Olive", type: "T-shirt" },
-//     { id: 17, size: "S", color: "Black", type: "Hoodie" },
-//     { id: 18, size: "M", color: "Black", type: "Hoodie" },
-//     { id: 19, size: "L", color: "Black", type: "Hoodie" },
-//     { id: 20, size: "XL", color: "Black", type: "Hoodie" },
-//     { id: 21, size: "S", color: "Blue", type: "Hoodie" },
-//     { id: 22, size: "M", color: "Blue", type: "Hoodie" },
-//     { id: 23, size: "L", color: "Blue", type: "Hoodie" },
-//     { id: 24, size: "XL", color: "Blue", type: "Hoodie" },
-//     { id: 25, size: "S", color: "Red", type: "Hoodie" },
-//     { id: 26, size: "M", color: "Red", type: "Hoodie" },
-//     { id: 27, size: "L", color: "Red", type: "Hoodie" },
-//     { id: 28, size: "XL", color: "Red", type: "Hoodie" },
-//     { id: 29, size: "S", color: "Olive", type: "Hoodie" },
-//     { id: 30, size: "M", color: "Olive", type: "Hoodie" },
-//     { id: 31, size: "L", color: "Olive", type: "Hoodie" },
-//     { id: 32, size: "XL", color: "Olive", type: "Hoodie" },
-//     { id: 36, size: "one-size", color: "Olive", type: "cap" },
-//     { id: 37, size: "one-size", color: "Olive", type: "cap" },
-//     { id: 38, size: "one-size", color: "Olive", type: "cap" },
-//     { id: 39, size: "one-size", color: "Olive", type: "cap" },
-//     { id: 40, size: "one-size", color: "Black", type: "cap" },
-//     { id: 41, size: "one-size", color: "Black", type: "cap" },
-//     { id: 42, size: "one-size", color: "Black", type: "cap" },
-//     { id: 43, size: "one-size", color: "Black", type: "cap" },
-//     { id: 44, size: "one-size", color: "Red", type: "cap" },
-//     { id: 45, size: "one-size", color: "Red", type: "cap" },
-//     { id: 46, size: "one-size", color: "Red", type: "cap" },
-//     { id: 47, size: "one-size", color: "Red", type: "cap" },
-//     { id: 47, size: "one-size", color: "Red", type: "cap" },
-// ];
-
 const products = [
     {
         id: 1,
@@ -268,168 +220,110 @@ const products = [
         id: 33,
         size: "one-size",
         color: "Olive",
-        type: "cap",
+        type: "Cap",
         url: "../../resources/img/merch/cap/olive-cap.jpg",
     },
     {
         id: 34,
         size: "one-size",
         color: "Black",
-        type: "cap",
+        type: "Cap",
         url: "../../resources/img/merch/cap/black-cap.jpg",
     },
     {
         id: 35,
         size: "one-size",
         color: "Red",
-        type: "cap",
+        type: "Cap",
         url: "../../resources/img/merch/cap/red-cap.jpg",
     },
 ];
 
-// Implement your filtering function:
-let productsFiltered = (value) => {
-    // Check if the value is present in the object
-    if (products.some((item) => Object.values(item).includes(`${value}`))) {
-        return products.filter((item) =>
-            Object.values(item).includes(`${value}`)
-        );
-    } else {
-        console.log(
-            "Property or value does not exist. Product does not exist."
-        );
-        return [];
-    }
-};
-
-// Implement sorting function with Array.toSorted()
-let productsSorted = (property) => {
-    if (property === "color") {
-        return products.toSorted((a, b) => a.color.localeCompare(b.color));
-    } else if (property === "size") {
-        const sizeOrder = ["S", "M", "L", "XL"];
-        const compareBySize = (a, b) => {
-            const sizeA = sizeOrder.indexOf(a.size);
-            const sizeB = sizeOrder.indexOf(b.size);
-
-            return sizeA - sizeB;
-        };
-        return products.toSorted(compareBySize);
-    }
-};
-
-// 2. Loop through your array to create the content of your page. For each item in
-// the list:
-// a. Create an element
-// b. Attach any properties or attributes you wish to it
-// c. Append any child elements you desire (make sure to create them first!)
-// d. Set the text content!
-
 document.addEventListener("DOMContentLoaded", function () {
-    const thumbnailContainer = document.getElementById(
-        "display-products-thumbnails"
-    );
     // create the thumbnail gallery from the whole product array of objects
-    products.forEach(function (product) {
-        console.log("product", product);
-        console.log("size", product.size);
-        const thumbnail = document.createElement("div");
-        thumbnail.id = thumbnail;
 
-        const thumbnailImage = document.createElement("img");
-        thumbnailImage.setAttribute("src", `${product.url}`);
-        thumbnailImage.setAttribute("alt", `${product.color} ${product.type}`);
+    let displayThumbnails = (productsArray) => {
+        const thumbnailContainer = document.getElementById(
+            "display-products-thumbnails"
+        );
+        productsArray.forEach(function (product) {
+            const thumbnail = document.createElement("div");
+            thumbnail.id = "thumbnail";
 
-        const thumbnailText = document.createElement("p");
-        thumbnailText.textContent = `size: ${product.size}`;
+            const thumbnailImage = document.createElement("img");
+            thumbnailImage.setAttribute("src", `${product.url}`);
+            thumbnailImage.setAttribute(
+                "alt",
+                `${product.color} ${product.type}`
+            );
 
-        thumbnail.appendChild(thumbnailImage);
-        thumbnail.appendChild(thumbnailText);
+            const thumbnailText = document.createElement("p");
+            thumbnailText.textContent = `size: ${product.size}`;
 
-        thumbnailContainer.appendChild(thumbnail);
+            thumbnail.appendChild(thumbnailImage);
+            thumbnail.appendChild(thumbnailText);
+
+            thumbnailContainer.appendChild(thumbnail);
+        });
+    };
+
+    displayThumbnails(products);
+
+    // define filtering function returning an filtered array of products
+
+    let productsFiltered = (value) => {
+        // Check if the value is present in the object
+        if (products.some((item) => Object.values(item).includes(`${value}`))) {
+            return products.filter((item) =>
+                Object.values(item).includes(`${value}`)
+            );
+        } else {
+            console.log(
+                "Property or value does not exist. Product does not exist."
+            );
+            return [];
+        }
+    };
+
+    //define sorting function returning a sorted array of products
+    let productsSorted = (property) => {
+        if (property === "color") {
+            return products.toSorted((a, b) => a.color.localeCompare(b.color));
+        } else if (property === "size") {
+            const sizeOrder = ["S", "M", "L", "XL"];
+            const compareBySize = (a, b) => {
+                const sizeA = sizeOrder.indexOf(a.size);
+                const sizeB = sizeOrder.indexOf(b.size);
+
+                return sizeA - sizeB;
+            };
+            return products.toSorted(compareBySize);
+        }
+    };
+
+    // Add event handlers to dropdown menu options to apply filtering and sorting:
+
+    // Filter by color:
+    document.getElementById("color-select").addEventListener("change", (e) => {
+        const colorSelected = e.target.value;
+        let filteredByColor = productsFiltered(colorSelected);
+        console.log(filteredByColor);
+    });
+    // Filter by size:
+    document.getElementById("size-select").addEventListener("change", (e) => {
+        const sizeSelected = e.target.value;
+        let filteredBySize = productsFiltered(sizeSelected);
+        console.log(filteredBySize);
+        displayThumbnails(filteredBySize);
+    });
+
+    // Filter by type:
+    document.getElementById("type-select").addEventListener("change", (e) => {
+        const typeSelected = e.target.value;
+        let filteredByType = productsFiltered(typeSelected);
+        console.log(filteredByType);
+        displayThumbnails(filteredByType);
     });
 });
 
-// let mapTable = (arr) => {
-
-//     const table = document.getElementById("product-table-body");
-
-//     let rows = arr.map(
-//         (elem) => `
-//         <tr>
-//           <td>${product.id}</td>
-//           <td>${product.size}</td>
-//           <td>${product.color}</td>
-//           <td>${product.type}</td>
-//         </tr>
-//       `
-//     );
-
-//     table.innerHTML = rows.join("");
-// };
-
-// console.log("Table Map", mapTable());
-
-//  Add some buttons at the top to filter your items, for example, to show only
-// dogs or cats.
-// 4. Add some buttons at the top to sort your items, for example by age, or by name
-// (alphabetically).
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const colorSelect = document.getElementById("color-select");
-//     const sizeSelect = document.getElementById("size-select");
-//     const typeSelect = document.getElementById("type-select");
-//     const sortSelect = document.getElementById("sort-select");
-//     const productGallery = document.querySelector(".product-gallery");
-
-//     colorSelect.addEventListener("change", filterProducts);
-//     sizeSelect.addEventListener("change", filterProducts);
-//     typeSelect.addEventListener("change", filterProducts);
-//     sortSelect.addEventListener("change", sortProducts);
-
-//     function filterProducts() {
-//         const colorFilter = colorSelect.value;
-//         const sizeFilter = sizeSelect.value;
-//         const typeFilter = typeSelect.value;
-
-//         const productItems = productGallery.querySelectorAll(".product-item");
-//         productItems.forEach(function (item) {
-//             const itemColor = item.getAttribute("data-color");
-//             const itemSize = item.getAttribute("data-size");
-//             const itemType = item.getAttribute("data-type");
-
-//             if (
-//                 (colorFilter === "all" || itemColor === colorFilter) &&
-//                 (sizeFilter === "all" || itemSize === sizeFilter) &&
-//                 (typeFilter === "all" || itemType === typeFilter)
-//             ) {
-//                 item.style.display = "block";
-//             } else {
-//                 item.style.display = "none";
-//             }
-//         });
-//     }
-
-//     function sortProducts() {
-//         const sortOption = sortSelect.value;
-//         const productItems = Array.from(
-//             productGallery.querySelectorAll(".product-item")
-//         );
-
-//         if (sortOption === "name") {
-//             productItems.sort((a, b) => {
-//                 const nameA = a.querySelector("h3").textContent;
-//                 const nameB = b.querySelector("h3").textContent;
-//                 return nameA.localeCompare(nameB);
-//             });
-//         } else if (sortOption === "price") {
-//             productItems.sort((a, b) => {
-//                 const priceA = parseFloat(a.getAttribute("data-price"));
-//                 const priceB = parseFloat(b.getAttribute("data-price"));
-//                 return priceA - priceB;
-//             });
-//         }
-
-//         productItems.forEach((item) => productGallery.appendChild(item));
-//     }
-// });
+// do the above with map() and innerHTML
