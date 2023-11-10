41485e3b9b719ff31e5d5d450c92a1a35dd637d3
onurtufekci34 <onurtufekci61@gmail.com>
2023-11-10 15:58:53 +0100

    addToCard functionality, saving localStorage, reading localStorage,shopping cart added, showing shopping cart, alertifyjs added

--- product_gallery/products.js
+++ product_gallery/products.js
@@ -1,4 +1,3 @@
-
 const array = [
   {
     id: 1,
@@ -127,7 +126,7 @@ const array = [
     },
   },
 ];
-
+ 
 // Function to create and populate cards
 function createCards() {
   const cardContainer = document.getElementById("card-container");
@@ -153,10 +152,11 @@ function createCards() {
           <div class="d-flex justify-content-end">
             <p class="card-text fs-3 text-danger">${item.price} €</p>
           </div>
-          
-          <div class="cardbtn d-flex justify-content-center">
-            <a href="#" class="btn btn-primary">add cart</a>
-          </div>
+
+          <form class="d-flex flex-column">
+          <input class="w-25 align-self-center fs-3 text-center" type="number" name="number" min="1" value="1" id="productQuantity-${item.id}">
+          <button type="button" class="btn btn-primary mt-2" onclick="addToCart(${item.id})">Add to Cart</button>
+        </form>
         </div>
       </div>
     `;
@@ -168,25 +168,67 @@ function createCards() {
 // Call the createCards function to populate the cards
 createCards();
 
+// Add to Cart function
+function addToCart(productId) {
+ 
+  // success notification
+// Shorthand for:
+// alertify.notify( message, 'success', [wait, callback]);
+alertify.success('Product added to cart');
 
-// reseting button       
-document.getElementById("reset").addEventListener("click", function (){
+
+
+  const quantityInput = document.getElementById(`productQuantity-${productId}`);
+  const quantity = parseInt(quantityInput.value);
+
+  if (quantity > 0) {
+    // Check if the product is already in localStorage
+    let cartItems = localStorage.getItem('cartItems');
+    cartItems = cartItems ? JSON.parse(cartItems) : [];
+
+    // Check if the product is already in the cart
+    const existingItem = cartItems.find(item => item.id === productId);
+
+    if (existingItem) {
+      // Update the quantity if the product is already in the cart
+      existingItem.quantity += quantity;
+    } else {
+      // Add the product to the cart if it's not already there
+      const product = array.find(item => item.id === productId);
+      cartItems.push({ id: productId, quantity, title: product.title, price: product.price,image:product.image });
+    }
+
+    // Save the updated cartItems to localStorage
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+
+   
+    
+  //  // Sayfayı yeniden yükle
+  //  location.reload();
+ 
+}
+
+}
+
+// reseting button
+document.getElementById("reset").addEventListener("click", function () {
   const cardContainer = document.getElementById("card-container");
-cardContainer.innerHTML = ""; // clear the cards
-createCards();
-})
+  cardContainer.innerHTML = ""; // clear the cards
+  createCards();
+});
 
 // "Business Laptops" button
 document.getElementById("business").addEventListener("click", function () {
-const cardContainer = document.getElementById("card-container");
-cardContainer.innerHTML = ""; // clear the cards
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = ""; // clear the cards
 
-array.forEach((item) => {
-if (item.category === "business laptop") {
-const cardDiv = document.createElement("div");
-cardDiv.className =
-  "col-sm-4 d-flex align-items-center justify-content-center mb-4";
-cardDiv.innerHTML = `
+  array.forEach((item) => {
+    if (item.category === "business laptop") {
+      const cardDiv = document.createElement("div");
+      cardDiv.className =
+        "col-sm-4 d-flex align-items-center justify-content-center mb-4";
+      cardDiv.innerHTML = `
   <div class="card" style="width: 20rem">
     <div class="cardimage d-flex justify-content-center">
       <img src="${item.image}" class="card-img-top w-50" alt="..." />
@@ -210,22 +252,22 @@ cardDiv.innerHTML = `
   </div>
 `;
 
-cardContainer.appendChild(cardDiv);
-}
-});
+      cardContainer.appendChild(cardDiv);
+    }
+  });
 });
 
-// geming laptops
+// gaming laptops
 document.getElementById("gaming").addEventListener("click", function () {
-const cardContainer = document.getElementById("card-container");
-cardContainer.innerHTML = ""; // clear the cards
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = ""; // clear the cards
 
-array.forEach((item) => {
-if (item.category === "gaming laptop") {
-const cardDiv = document.createElement("div");
-cardDiv.className =
-  "col-sm-4 d-flex align-items-center justify-content-center mb-4";
-cardDiv.innerHTML = `
+  array.forEach((item) => {
+    if (item.category === "gaming laptop") {
+      const cardDiv = document.createElement("div");
+      cardDiv.className =
+        "col-sm-4 d-flex align-items-center justify-content-center mb-4";
+      cardDiv.innerHTML = `
   <div class="card" style="width: 20rem">
     <div class="cardimage d-flex justify-content-center">
       <img src="${item.image}" class="card-img-top w-50" alt="..." />
@@ -249,26 +291,26 @@ cardDiv.innerHTML = `
   </div>
 `;
 
-cardContainer.appendChild(cardDiv);
-}
-});
+      cardContainer.appendChild(cardDiv);
+    }
+  });
 });
 // "Increasing Price" button
 document.getElementById("increasePrice").addEventListener("click", function () {
-const cardContainer = document.getElementById("card-container");
-cardContainer.innerHTML = ""; // Önce mevcut kartları temizle
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = ""; // Önce mevcut kartları temizle
 
-// sorting the array
-const sortedArray = array.slice().sort((a, b) => a.price - b.price);
+  // sorting the array
+  const sortedArray = array.slice().sort((a, b) => a.price - b.price);
 
-// clear the cardContainer'
-cardContainer.innerHTML = "";
+  // clear the cardContainer'
+  cardContainer.innerHTML = "";
 
-sortedArray.forEach((item) => {
-const cardDiv = document.createElement("div");
-cardDiv.className =
-"col-sm-4 d-flex align-items-center justify-content-center mb-4";
-cardDiv.innerHTML = `
+  sortedArray.forEach((item) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.className =
+      "col-sm-4 d-flex align-items-center justify-content-center mb-4";
+    cardDiv.innerHTML = `
 <div class="card" style="width: 20rem">
   <div class="cardimage d-flex justify-content-center">
     <img src="${item.image}" class="card-img-top w-50" alt="..." />
@@ -292,25 +334,25 @@ cardDiv.innerHTML = `
 </div>
 `;
 
-cardContainer.appendChild(cardDiv);
-});
+    cardContainer.appendChild(cardDiv);
+  });
 });
 // "Decreasing Price" button
 document.getElementById("decreasePrice").addEventListener("click", function () {
-const cardContainer = document.getElementById("card-container");
-cardContainer.innerHTML = ""; // clear the card
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = ""; // clear the card
 
-// sorting array
-const sortedArray = array.slice().sort((a, b) => b.price - a.price);
+  // sorting array
+  const sortedArray = array.slice().sort((a, b) => b.price - a.price);
 
-// clear the cardContainer
-cardContainer.innerHTML = "";
+  // clear the cardContainer
+  cardContainer.innerHTML = "";
 
-sortedArray.forEach((item) => {
-const cardDiv = document.createElement("div");
-cardDiv.className =
-"col-sm-4 d-flex align-items-center justify-content-center mb-4";
-cardDiv.innerHTML = `
+  sortedArray.forEach((item) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.className =
+      "col-sm-4 d-flex align-items-center justify-content-center mb-4";
+    cardDiv.innerHTML = `
 <div class="card" style="width: 20rem">
   <div class="cardimage d-flex justify-content-center">
     <img src="${item.image}" class="card-img-top w-50" alt="..." />
@@ -334,8 +376,12 @@ cardDiv.innerHTML = `
 </div>
 `;
 
-cardContainer.appendChild(cardDiv);
-});
+    cardContainer.appendChild(cardDiv);
+  });
 });
 
+
+
+  
+    
 
