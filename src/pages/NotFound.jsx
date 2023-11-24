b7dc46242c4282ec1f8dfac16d2779ee7c3bb42f
onurtufekci34 <onurtufekci61@gmail.com>
2023-11-24 17:38:43 +0100

    nt found page added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,16 @@
-
+import Notfoundgif from '../images/notfound.webp'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="container">
+    <div className="row">
+      <div className="col-md-6 offset-md-3 text-center mt-5">
+        <h1 className="display-4">404</h1>
+        <p className="lead">Oops! Page not found.</p>
+        <p>Sorry, the page you are looking for might have been removed or doesn't exist.</p>
+        <img src={Notfoundgif} alt="not found gif" />
+      </div>
+    </div>
+  </div>
   )
 }
